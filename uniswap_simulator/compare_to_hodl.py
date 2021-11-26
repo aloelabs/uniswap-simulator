Fix fee calculation on v2 position, simplify compare_to_hodl

uniswap_simulator/compare_to_hodl.py
@@ -4,7 +4,7 @@ import numpy as np
 INITIAL_INVENTORY0 = 10000
 
 
-def compare_to_hodl(strategy, prices, time):
+def compare_to_hodl(strategy, prices, T):
     # price trajectories should start from the same value (at t=0)
     assert prices[0].std() == 0.
     initial_price = prices[0].mean()
@@ -33,8 +33,8 @@ def compare_to_hodl(strategy, prices, time):
     y = values.copy()
 
     y = y.sum(axis=2) / hodl[0]
-    G_end_point = np.log(y[-1]).mean() / time[-1]
+    G_end_point = np.log(y[-1]).mean() / T
     y = hodl[-1] / hodl[0]
-    G_end_point_hodl = np.log(y).mean() / time[-1]
+    G_end_point_hodl = np.log(y).mean() / T
 
     return G_end_point, G_end_point_hodl

uniswap_simulator/gbm.py
@@ -16,7 +16,7 @@ class GeometricBrownianMotion:
             size=(self._n - 1, count)
         )
 
-        x = np.exp((self._mu - self._sigma ** 2 / 2) * self._dt + self._sigma * noise)
+        x = np.exp((self._mu - 0.5 * self._sigma ** 2) * self._dt + self._sigma * noise)
         x = np.pad(x, ((1, 0), (0, 0)), mode='constant', constant_values=1)
 
         return self._x0 * x.cumprod(axis=0)

uniswap_simulator/position_v2.py
@@ -7,7 +7,7 @@ class PositionV2:
     def __init__(self, price, fee):
         self._price_sqrt = np.sqrt(price)
         self._fee = fee
-        self._gamma = 1. + fee
+        self._gamma = 1. - fee
 
         self._k = np.zeros_like(price)
         self._x = np.zeros_like(price)
@@ -44,9 +44,9 @@ class PositionV2:
 
         mask = price > self._price # where price is growing
 
-        a = -self._x * (2. + self._fee) + np.sqrt(np.square(self._x * self._fee) + 4 * self._gamma * self._k / price)
+        a = -self._x * (2. - self._fee) + np.sqrt(np.square(self._x * self._fee) + 4 * self._gamma * self._k / price)
         a /= 2. * self._gamma
-        b = -self._y * (2. + self._fee) + np.sqrt(np.square(self._y * self._fee) + 4 * self._gamma * self._k * price)
+        b = -self._y * (2. - self._fee) + np.sqrt(np.square(self._y * self._fee) + 4 * self._gamma * self._k * price)
         b /= 2. * self._gamma
 
         self._x[~mask] += a[~mask]